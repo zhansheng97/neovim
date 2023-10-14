950c2bc307d0050cd5830daa494c174566983023
zhansheng <zhansheng.lee@3enix.com>
2023-10-14 23:24:23 +0800

    first commit

---
+++ lua/user/lualine.lua
@@ -0,0 +1,133 @@
+local status_ok, lualine = pcall(require, "lualine")
+if not status_ok then
+	return
+end
+
+local hide_in_width = function()
+	return vim.fn.winwidth(0) > 80
+end
+
+local colors = {
+	red = "#ec5f67",
+	yellow = "#ECBE7B",
+	cyan = "#008080",
+	darkblue = "#081633",
+	green = "#98be65",
+	orange = "#FF8800",
+	violet = "#a9a1e1",
+	magenta = "#c678dd",
+	blue = "#51afef",
+}
+
+local diagnostics = {
+	"diagnostics",
+	sources = { "nvim_diagnostic" },
+	sections = { "error", "warn" },
+	symbols = { error = " ", warn = " " },
+	colored = false,
+	update_in_insert = false,
+	always_visible = true,
+}
+
+local diff = {
+	"diff",
+	colored = true,
+	symbols = { added = " ", modified = " ", removed = " " }, -- changes diff symbols
+	cond = hide_in_width,
+}
+
+local mode = {
+	"mode",
+	fmt = function(str)
+		return "-- " .. str .. " --"
+	end,
+}
+
+local filetype = {
+	"filetype",
+	icons_enabled = true,
+	icon = nil,
+	colored = false,
+}
+
+local branch = {
+	"branch",
+	icons_enabled = true,
+	icon = "",
+}
+
+local location = {
+	"location",
+	padding = {
+		right = 1,
+	},
+}
+
+local fileformat = {
+	"fileformat",
+	fmt = function(str)
+		return str .. ""
+	end,
+}
+-- cool function for progress
+-- local progress = function()
+-- 	local current_line = vim.fn.line(".")
+-- 	local total_lines = vim.fn.line("$")
+-- 	local chars = { "__", "▁▁", "▂▂", "▃▃", "▄▄", "▅▅", "▆▆", "▇▇", "██" }
+-- 	local line_ratio = current_line / total_lines
+-- 	local index = math.ceil(line_ratio * #chars)
+-- 	return chars[index]
+-- end
+
+local progress = {
+	"progress",
+}
+
+local filename = {
+	"filename",
+	file_status = true, -- Displays file status (readonly status, modified status)
+	newfile_status = false, -- Display new file status (new file means no write after created)
+	path = 0, -- 0: Just the filename
+	shorting_target = 40, -- Shortens path to leave 40 spaces in the window
+	symbols = {
+		modified = "[+]", -- Text to show when the file is modified.
+		readonly = "[-]", -- Text to show when the file is non-modifiable or readonly.
+		unnamed = "[No Name]", -- Text to show for unnamed buffers.
+		newfile = "[New]", -- Text to show for newly created file before first write
+	},
+}
+
+local spaces = function()
+	return "spaces: " .. vim.api.nvim_buf_get_option(0, "shiftwidth")
+end
+
+lualine.setup({
+	options = {
+		icons_enabled = true,
+		theme = "auto",
+		component_separators = { left = "", right = "/" },
+		section_separators = { left = "", right = "" },
+		disabled_filetypes = { "alpha", "dashboard", "NvimTree", "Outline" },
+		always_divide_middle = true,
+	},
+	sections = {
+		lualine_a = { "mode" },
+		lualine_b = { branch },
+		lualine_c = { filename },
+		-- lualine_x = { "encoding", "fileformat", "filetype" },
+		-- lualine_x = { diff, spaces, "encoding", filetype },
+		lualine_x = { diff, "encoding", filetype, fileformat },
+		lualine_y = { location },
+		lualine_z = { progress },
+	},
+	inactive_sections = {
+		lualine_a = {},
+		lualine_b = {},
+		lualine_c = { filename },
+		lualine_x = { "location" },
+		lualine_y = {},
+		lualine_z = {},
+	},
+	tabline = {},
+	extensions = {},
+})
